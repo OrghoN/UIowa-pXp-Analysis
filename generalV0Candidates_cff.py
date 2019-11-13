e53de5c6e1a32bd242adc81ad5fbd49691a9bd4c
lgemedia <43244081+lgemedia@users.noreply.github.com>
2019-11-12 23:23:43 -0300

    Add files via upload

---
+++ generalV0Candidates_cff.py
@@ -0,0 +1,13 @@
+import FWCore.ParameterSet.Config as cms
+
+# magnetic field
+# tracker geometry
+# global tracker geometry
+#from Geometry.CommonDetUnit.globalTrackingGeometry_cfi import *
+# tracker geometry
+# tracker numbering
+# ctfV0Producer
+from RecoVertex.V0Producer.generalV0Candidates_cfi import *
+
+from Configuration.Eras.Modifier_pp_on_AA_2018_cff import pp_on_AA_2018
+pp_on_AA_2018.toModify(generalV0Candidates, tkPtCut = 999.)
